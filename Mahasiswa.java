862b9e0254a37091bf01c9cb0be79f3f3ac3d86c
maktal.sakriadhi <maktal.sakriadhi>
2018-09-07 14:22:58 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,14 @@
+package maxshady;
+
+public class Mahasiswa {
+
+	public Mahasiswa() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hey Max Shady");
+
+	}
+
+}
